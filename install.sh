9fb7945638be09c715142c79247130e1692932c5
david.sf026 <david.sf026@gmail.com>
2022-10-06 09:51:21 -0300

    Gitea is no more managed by Argo CD

--- install.sh
+++ install.sh
@@ -7,21 +7,19 @@ oc new-project oqss-hom
 oc new-project oqss-prod
 oc new-project oqss-cicd # deletar
 
-# APLICANDO LABEL PARA ADMINISTRAÇÃO DO ARGOCD
+# APLICANDO LABEL PARA ADMINISTRAÇÃO DO ARGOCD NOS NAMESPACES INSTANCIADOS ACIMA
 oc label namespace oqss-dev argocd.argoproj.io/managed-by=${ARGOCD_NS}
 oc label namespace oqss-hom argocd.argoproj.io/managed-by=${ARGOCD_NS}
 oc label namespace oqss-prod argocd.argoproj.io/managed-by=${ARGOCD_NS}
 oc label namespace oqss-cicd argocd.argoproj.io/managed-by=${ARGOCD_NS}
 
 # ATRIBUINDO A PERMISSÃO "EDIT" PARA A SERVICE ACCOUNT DO OPENSHIFT PIPELINES
-# (PERGUNTAR PARA O MAIDA PORQUE ELE USOU A SA PIPELINE DO PROJETO CICD, PORQUE ELE NÃO ENCONTRA ESSA SA. SERÁ QUE É PORQUE O PASSO ACIMA NÃO RODOU?)
 oc adm policy add-role-to-user edit system:serviceaccount:oqss-dev:pipeline -n oqss-dev
 oc adm policy add-role-to-user edit system:serviceaccount:oqss-hom:pipeline -n oqss-hom
 oc adm policy add-role-to-user edit system:serviceaccount:oqss-prod:pipeline -n oqss-prod
 oc adm policy add-role-to-user edit system:serviceaccount:oqss-prod:pipeline -n oqss-cicd
 
 # ATRIBUINDO A PERMISSÃO "IMAGE-PULLER" PARA A SERVICE ACCOUNT DO OPENSHIFT PIPELINES
-# (PERGUNTAR PARA O MAIDA PORQUE ELE USOU A SA PIPELINE DO PROJETO CICD, PORQUE ELE NÃO ENCONTRA ESSA SA. SERÁ QUE É PORQUE O PASSO ACIMA NÃO RODOU?)
 oc policy add-role-to-user system:image-puller system:serviceaccount:oqss-dev:pipeline -n oqss-dev
 oc policy add-role-to-user system:image-puller system:serviceaccount:oqss-hom:pipeline -n oqss-hom
 oc policy add-role-to-user system:image-puller system:serviceaccount:oqss-prod:pipeline -n oqss-prod
@@ -29,10 +27,10 @@ oc policy add-role-to-user system:image-puller system:serviceaccount:oqss-prod:p
 
 # INSTACIANDO APPLICATIONS NO ARGOCD
 
-# argocd repo add https://github.com/redhat-oraex/oqss-cnp-manifests --username davidsf026 --password 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
-# argocd repo add https://github.com/redhat-oraex/oqss-cnp-provisioning --username davidsf026 --password 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
+# argocd repo add https://github.com/redhat-oraex/oqss-cnp-gitops --username davidsf026 --password ************************
+# argocd repo add https://github.com/redhat-oraex/oqss-cnp-argocd-provisioning --username davidsf026 --password ************************
 
-argocd app create -f oqss-cicd/gitea-bootstrap.yaml
+argocd app create -f oqss-cicd/gitea.yaml # APENAS PARA DEMONSTRAÇÃO, EM CLIENTES ELES CERTAMENTE JÁ TERÃO UM SERVIDOR GIT
 
 sleep 10
 
@@ -42,5 +40,3 @@ oc wait --for=condition=complete job/${SETUP_GITEA_JOB_NAME} --timeout=600s
 argocd app create -f oqss-cicd/nexus.yaml
 argocd app create -f oqss-cicd/sonarqube.yaml
 argocd app create -f oqss-cicd/pipeline.yaml
-
-argocd app patch gitea --patch '{"spec": { "source": { "repoURL": "http://gitea-oqss-cicd.apps.middleware.rhbr-lab.com/gitea-admin/oqss-cnp-manifests" } }}' --type merge
